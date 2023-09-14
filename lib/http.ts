fcf390eb8b1a1d4c3f5538301600909e80543e18
garystrousers <ben.m.lee@outlook.com>
2023-09-14 13:37:20 +1000

    [add] filetypes list

--- lib/http.ts
+++ lib/http.ts
@@ -2,12 +2,12 @@ import { readdirSync } from "fs";
 
 let templates = new Map<string, string>();
 
-export async function loadTemplates(dir:string) {
+export async function loadTemplates(dir: string) {
   for (const filename of readdirSync(dir)) {
     let file = Bun.file(`${dir}/${filename}`)
     templates.set(filename.replace('.html', ''), await file.text())
   }
-} 
+}
 
 export function html(page: string) {
   return new Response(page, {
@@ -15,7 +15,7 @@ export function html(page: string) {
   });
 }
 
-export async function json(data:any) {
+export async function json(data: any) {
   return new Response(JSON.stringify(data), {
     headers: { 'Content-Type': 'text/json' }
   })
@@ -42,11 +42,88 @@ export async function page(filename: string) {
   return await file.text()
 }
 
-function getContentType(filename:string):string {
+function getContentType(filename: string): string {
   let type = filename.match(/.+\.(.+)$/i)
   if (!type) return 'idk';
-  let filetype = type[1].toLowerCase()
-  if (filetype == 'png') return 'image/png'
-  if (filetype == 'ico') return 'image/ico'
-  return 'idk';
+  let filetype = types.get(type[1].toLowerCase())
+  return filetype ? filetype : "text/plain"
 }
+
+const types = new Map<string, string>([
+  [".aac", "audio/aac"],
+  [".abw", "application/x-abiword"],
+  [".arc", "application/x-freearc"],
+  [".avif", "image/avif"],
+  [".avi", "video/x-msvideo"],
+  [".azw", "application/vnd.amazon.ebook"],
+  [".bin", "application/octet-stream"],
+  [".bmp", "image/bmp"],
+  [".bz", "application/x-bzip"],
+  [".bz2", "application/x-bzip2"],
+  [".cda", "application/x-cdf"],
+  [".csh", "application/x-csh"],
+  [".css", "text/css"],
+  [".csv", "text/csv"],
+  [".doc", "application/msword"],
+  [".docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document"],
+  [".eot", "application/vnd.ms-fontobject"],
+  [".epub", "application/epub+zip"],
+  [".gz", "application/gzip"],
+  [".gif", "image/gif"],
+  [".htm", "text/html"],
+  [".html", "text/html"],
+  [".ico", "image/vnd.microsoft.icon"],
+  [".ics", "text/calendar"],
+  [".jar", "application/java-archive"],
+  [".jpeg", "image/jpeg"],
+  [".jpg", "image/jpeg"],
+  [".js", "text/javascript"],
+  [".json", "application/json"],
+  [".jsonld", "application/ld+json"],
+  [".mid", "audio/midi"],
+  [".midi", "audio/midi"],
+  [".mjs", "text/javascript"],
+  [".mp3", "audio/mpeg"],
+  [".mp4", "video/mp4"],
+  [".mpeg", "video/mpeg"],
+  [".mpkg", "application/vnd.apple.installer+xml"],
+  [".odp", "application/vnd.oasis.opendocument.presentation"],
+  [".ods", "application/vnd.oasis.opendocument.spreadsheet"],
+  [".odt", "application/vnd.oasis.opendocument.text"],
+  [".oga", "audio/ogg"],
+  [".ogv", "video/ogg"],
+  [".ogx", "application/ogg"],
+  [".opus", "audio/opus"],
+  [".otf", "font/otf"],
+  [".png", "image/png"],
+  [".pdf", "application/pdf"],
+  [".php", "application/x-httpd-php"],
+  [".ppt", "application/vnd.ms-powerpoint"],
+  [".pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation"],
+  [".rar", "application/vnd.rar"],
+  [".rtf", "application/rtf"],
+  [".sh", "application/x-sh"],
+  [".svg", "image/svg+xml"],
+  [".tar", "application/x-tar"],
+  [".tif", "image/tiff"],
+  [".tiff", "image/tiff"],
+  [".ts", "video/mp2t"],
+  [".ttf", "font/ttf"],
+  [".txt", "text/plain"],
+  [".vsd", "application/vnd.visio"],
+  [".wav", "audio/wav"],
+  [".weba", "audio/webm"],
+  [".webm", "video/webm"],
+  [".webp", "image/webp"],
+  [".woff", "font/woff"],
+  [".woff2", "font/woff2"],
+  [".xhtml", "application/xhtml+xml"],
+  [".xls", "application/vnd.ms-excel"],
+  [".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"],
+  [".xml", "application/xml"],
+  [".xul", "application/vnd.mozilla.xul+xml"],
+  [".zip", "application/zip"],
+  [".3gp", "video/3gpp"],
+  [".3g2", "video/3gpp2"],
+  [".7z", "application/x-7z-compressed"]
+]);
